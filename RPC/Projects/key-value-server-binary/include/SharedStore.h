141578449651ec83550734128aff96f0a9aba9fe
Stephoon15s <steventres7@gmail.com>
2026-06-03 13:51:43 -0700

    Unsure if I need to replace the vector or what

--- RPC/Projects/key-value-server-binary/include/SharedStore.h
+++ RPC/Projects/key-value-server-binary/include/SharedStore.h
@@ -1,6 +1,9 @@
 #pragma once
+// HW 2 Includes
+#include <unordered_map>
 
 struct SharedStore {
     std::vector<std::string> values{};
+    std::unordered_map<std::string, std::string> store{}; // Replaces the vector?? 
     std::mutex mutex{};
 };
