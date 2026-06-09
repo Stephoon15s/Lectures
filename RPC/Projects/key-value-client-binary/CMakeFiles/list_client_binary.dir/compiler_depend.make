387e76a83043c2e9e25dcab95cb4cb4c7f62adf5
Stephoon15s <steventres7@gmail.com>
2026-06-09 12:05:49 -0700

    Clear, I'm redoing it

--- RPC/Projects/key-value-client-binary/CMakeFiles/list_client_binary.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for list_client_binary.
-# This may be replaced when dependencies are built.
--- RPC/Projects/key-value-server-binary/include/SharedStore.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// HW 2 Includes
-#include <unordered_map>
-
-struct SharedStore {
-    std::unordered_map<std::string, std::string> values{}; // Replaces the vector
-    std::mutex mutex{};
-};
